340cd73c965fdd2d3eeb8f760a73bc87cc94ae0e
HubertOrzełowski <s20467@pjwstk.edu.pl>
2020-05-18 13:58:58 +0200

    Initial commit

---
+++ src/Board.java
@@ -0,0 +1,376 @@
+public class Board {
+    boolean lost;
+    int points = 0;
+    boolean[][] arr;
+    Block block;
+
+    class Position {
+        int x;
+        int y;
+        public void copy(Position toCpy) {x = toCpy.getX(); y = toCpy.getY();}
+        public Position(int x, int y) { this.x=x; this.y=y; }
+        public void setY(int y) { this.y = y; }
+        public void setX(int x) { this.x = x; }
+        public int getX() { return x; }
+        public int getY() { return y; }
+    }
+
+    public Board(){
+        arr = new boolean[16][8];
+        lost=false;
+        block = new Block();
+    }
+
+    public boolean isLost() {
+        return lost;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void moveRight(){
+        boolean[][] blockArr = block.getFullArray();
+        for(int i=0; i<16; i++){
+            if(blockArr[i][7])
+                return;
+        }
+        for(int i=0; i<16; i++){
+            for(int j=6; j>=0; j--){
+                if(blockArr[i][j])
+                    blockArr[i][j + 1] = true;
+            }
+        }
+        if(ifCollision(blockArr))
+            return;
+        block.moveRight();
+    }
+
+    public void moveLeft(){
+        boolean[][] blockArr = block.getFullArray();
+        for(int i=0; i<16; i++){
+            if(blockArr[i][0])
+                return;
+        }
+        boolean[][] tmp = new boolean[16][8];
+
+        for(int i=0; i<16; i++){
+            for(int j=1; j<8; j++){
+                if(blockArr[i][j])
+                    tmp[i][j - 1] = true;
+            }
+        }
+        if(ifCollision(tmp))
+            return;
+        block.moveLeft();
+    }
+
+    public void moveDown(){
+        boolean[][] blockArr = block.getFullArray();
+
+        for(int i=0; i<8; i++){
+            if(blockArr[15][i]) {
+                for(int k=0; k<16; k++){
+                    for(int l=0; l<8; l++){
+                        arr[k][l] = arr[k][l] || blockArr[k][l];
+                    }
+                }
+                checkIfPoints();
+                generateBlock();
+                return;
+            }
+        }
+
+        boolean[][] tmp = new boolean[16][8];
+
+        for(int i=14; i>0; i--){
+            for(int j=0; j<8; j++){
+                if(blockArr[i][j])
+                    tmp[i + 1][j] = true;
+            }
+        }
+        if(ifCollision(tmp)) {
+            for (int k = 0; k < 16; k++) {
+                for (int l = 0; l < 8; l++) {
+                    arr[k][l] = arr[k][l] || blockArr[k][l];
+                    }
+                }
+            checkIfPoints();
+            generateBlock();
+            return;
+        }
+
+        block.moveDown();
+
+        for(int i=0; i<8; i++){
+            if(blockArr[15][i]) {
+                for(int k=0; k<16; k++){
+                    for(int l=0; l<8; l++){
+                        arr[k][l] = arr[k][l] || blockArr[k][l];
+                    }
+                }
+                checkIfPoints();
+                generateBlock();
+                return;
+            }
+        }
+    }
+
+    public void rotate(){
+        if(block.blockPosition.getX()<0 || block.blockPosition.getX()>5)
+            return;
+        Block tmp = new Block();
+        tmp.copy(block);
+        tmp.rotate();
+        if(!(ifCollision(tmp.getFullArray())))
+            block.rotate();
+    }
+
+    public boolean ifCollision(boolean[][] tmp) {
+        for(int i=0; i<16; i++) {
+            for (int j = 0; j < 8; j++) {
+                if(tmp[i][j] && arr[i][j])
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public void generateBlock(){
+        block = new Block();
+        if(ifCollision(block.getFullArray()))
+            lost=true;
+        for(int i=0; i<2; i++){
+            for(int j=0; j<8; j++){
+                if(arr[i][j]) {
+                    lost = true;
+                }
+            }
+        }
+    }
+
+    public void checkIfPoints(){
+        int counter=0;
+        for(int i=0; i<16; i++){
+            counter=0;
+            for(int j=0; j<8; j++){
+                if(arr[i][j])
+                    counter++;
+            }
+            if(counter==8) {
+                deleteRow(i);
+                points++;
+            }
+        }
+    }
+
+    public void deleteRow(int nr){
+        for(int i=nr; i>0; i--){
+            for(int j=0; j<8; j++){
+                arr[i][j] = arr[i-1][j];
+            }
+        }
+        for(int i=0; i<8; i++)
+            arr[0][i]=false;
+    }
+
+    public boolean[][] getBoard(){
+        boolean[][] blockArr = block.getFullArray();
+        for(int i=0; i<16; i++){
+            for(int j=0; j<8; j++) {
+                blockArr[i][j]=(arr[i][j] || blockArr[i][j]);
+            }
+        }
+        return blockArr;
+    }
+
+
+    class Block{
+        Position blockPosition;    //left-upper corner of bounding square 3x3
+        String blockShape;
+        boolean[][] block;
+        int actualRotation;
+        boolean[][][] LBlocksRotations;
+        boolean[][][] IBlocksRotations;
+        boolean[][][] TBlocksRotations;
+        boolean[][][] SquareBlocksRotations;
+        public Block(){
+            initBlocksRotations();
+            block = new boolean[3][3];
+            int rndm = (int)(Math.random()*4);
+            switch(rndm){
+                case 0:
+                    blockPosition = new Position(0, 0);
+                    actualRotation = 0;
+                    blockShape = "L";
+                    for(int i=0; i<3; i++){
+                        for(int j=0; j<3; j++){
+                            block[i][j] = LBlocksRotations[0][i][j];
+                        }
+                    }
+                    break;
+                case 1:
+                    blockPosition = new Position(0, 0);
+                    actualRotation = 0;
+                    blockShape = "I";
+                    for(int i=0; i<3; i++){
+                        for(int j=0; j<3; j++){
+                            block[i][j] = IBlocksRotations[0][i][j];
+                        }
+                    }
+                    break;
+                case 2:
+                    blockPosition = new Position(0, 0);
+                    actualRotation = 0;
+                    blockShape = "T";
+                    for(int i=0; i<3; i++){
+                        for(int j=0; j<3; j++){
+                            block[i][j] = TBlocksRotations[0][i][j];
+                        }
+                    }
+                    break;
+                case 3:
+                    blockPosition = new Position(0, 0);
+                    actualRotation = 0;
+                    blockShape = "Square";
+                    for(int i=0; i<3; i++){
+                        for(int j=0; j<3; j++){
+                            block[i][j] = SquareBlocksRotations[0][i][j];
+                        }
+                    }
+                    break;
+            }
+        }
+
+        public void rotate(){
+            switch(blockShape){
+                case "L":
+                    block = LBlocksRotations[(actualRotation+1)%4];
+                    actualRotation=(actualRotation+1)%4;
+                    break;
+                case "I":
+                    block = IBlocksRotations[(actualRotation+1)%4];
+                    actualRotation=(actualRotation+1)%4;
+                    break;
+                case "T":
+                    block = TBlocksRotations[(actualRotation+1)%4];
+                    actualRotation=(actualRotation+1)%4;
+                    break;
+                case "Square":
+                    break;
+            }
+        }
+
+        public void copy(Block toCpy){
+            blockPosition.copy(toCpy.getBlockPosition());
+            blockShape = toCpy.blockShape;
+            for(int i=0; i<3; i++){
+                for(int j=0; j<3; j++){
+                    block[i][j]=toCpy.block[i][j];
+                }
+            }
+            actualRotation = toCpy.actualRotation;
+        }
+
+        public void moveRight(){
+            blockPosition.setX(blockPosition.getX()+1);
+        }
+        public void moveLeft(){
+            blockPosition.setX(blockPosition.getX()-1);
+        }
+        public void moveDown(){
+            blockPosition.setY(blockPosition.getY()+1);
+        }
+        public boolean[][] getFullArray(){
+            boolean[][] fullArray = new boolean[16][8];
+            for(int i=0; i<3; i++){
+                for(int j=0; j<3; j++){
+                    if(block[i][j])
+                        fullArray[i + blockPosition.getY()][j + blockPosition.getX()] = true;
+                }
+            }
+            return fullArray;
+        }
+
+        public void initBlocksRotations(){
+            LBlocksRotations = new boolean[4][3][3];
+            //rotation 0
+                LBlocksRotations[0][0][1]=true;
+                LBlocksRotations[0][1][1]=true;
+                LBlocksRotations[0][2][1]=true;
+                LBlocksRotations[0][2][2]=true;
+            //rotation 1
+                LBlocksRotations[1][1][0]=true;
+                LBlocksRotations[1][1][1]=true;
+                LBlocksRotations[1][1][2]=true;
+                LBlocksRotations[1][2][0]=true;
+            //rotation 2
+                LBlocksRotations[2][0][1]=true;
+                LBlocksRotations[2][1][1]=true;
+                LBlocksRotations[2][2][1]=true;
+                LBlocksRotations[2][0][0]=true;
+            //rotation 3
+                LBlocksRotations[3][1][0]=true;
+                LBlocksRotations[3][1][1]=true;
+                LBlocksRotations[3][1][2]=true;
+                LBlocksRotations[3][0][2]=true;
+
+            TBlocksRotations = new boolean[4][3][3];
+            //rotation 0
+                TBlocksRotations[0][1][0]=true;
+                TBlocksRotations[0][1][1]=true;
+                TBlocksRotations[0][1][2]=true;
+                TBlocksRotations[0][2][1]=true;
+            //rotation 1
+                TBlocksRotations[1][0][1]=true;
+                TBlocksRotations[1][1][1]=true;
+                TBlocksRotations[1][2][1]=true;
+                TBlocksRotations[1][1][0]=true;
+            //rotation 2
+                TBlocksRotations[2][1][0]=true;
+                TBlocksRotations[2][1][1]=true;
+                TBlocksRotations[2][1][2]=true;
+                TBlocksRotations[2][0][1]=true;
+            //rotation 3
+                TBlocksRotations[3][0][1]=true;
+                TBlocksRotations[3][1][1]=true;
+                TBlocksRotations[3][2][1]=true;
+                TBlocksRotations[3][1][2]=true;
+
+            IBlocksRotations = new boolean[4][3][3];
+            //rotation 0
+                IBlocksRotations[0][0][1]=true;
+                IBlocksRotations[0][1][1]=true;
+                IBlocksRotations[0][2][1]=true;
+            //rotation 1
+                IBlocksRotations[1][1][0]=true;
+                IBlocksRotations[1][1][1]=true;
+                IBlocksRotations[1][1][2]=true;
+            //rotation 2
+                IBlocksRotations[2][0][1]=true;
+                IBlocksRotations[2][1][1]=true;
+                IBlocksRotations[2][2][1]=true;
+            //rotation 3
+                IBlocksRotations[3][1][0]=true;
+                IBlocksRotations[3][1][1]=true;
+                IBlocksRotations[3][1][2]=true;
+
+            SquareBlocksRotations = new boolean[4][3][3];
+            //rotation 0
+                SquareBlocksRotations[0][0][0]=true;
+                SquareBlocksRotations[0][1][0]=true;
+                SquareBlocksRotations[0][0][1]=true;
+                SquareBlocksRotations[0][1][1]=true;
+            //rotation 1,2,3
+                SquareBlocksRotations[3]=SquareBlocksRotations[2]=SquareBlocksRotations[1]=SquareBlocksRotations[0];
+
+
+        }
+
+        public Position getBlockPosition() {
+            return blockPosition;
+        }
+
+    }
+
+}
---
+++ src/Tetris.java
@@ -0,0 +1,186 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.*;
+
+import static java.lang.Thread.sleep;
+
+public class Tetris extends JFrame{
+    int screenHeight;
+    int screenWidth;
+    int highScore;
+    int score;
+    BoardPanel boardPanel;
+    Board board;
+    JButton rozpocznij;
+    JLabel up;
+    JLabel down;
+    JLabel left;
+    JLabel right;
+    JLabel highestScore;
+    Box menuBox;
+
+    void menu(){
+        setTitle("TETRIS");
+        setSize(300, 270);
+        setLocation((screenWidth-this.getWidth())/2, (screenHeight-this.getHeight())/2);
+        //setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        menuInit();
+        setVisible(true);
+    }
+
+    void menuInit(){
+        rozpocznij = new JButton("Start");
+        up = new JLabel("Strzalka w gore, aby obrocic blok");
+        down = new JLabel("Strzalka w dol, aby obnizyc blok");
+        right = new JLabel("Strzalka w prawo, aby przesunac blok w prawo");
+        left = new JLabel("Strzalka w lewo, aby przesunac blok w lewo");
+        highestScore = new JLabel("najwyzszy wynik: "+ highScore);
+        menuBox = Box.createVerticalBox();
+        menuBox.add(Box.createVerticalStrut(15));
+        menuBox.add(rozpocznij);
+        rozpocznij.setAlignmentX(Component.CENTER_ALIGNMENT);
+        menuBox.add(Box.createVerticalStrut(15));
+        menuBox.add(highestScore);
+        highestScore.setAlignmentX(Component.CENTER_ALIGNMENT);
+        menuBox.add(Box.createVerticalStrut(15));
+        menuBox.add(up);
+        up.setAlignmentX(Component.CENTER_ALIGNMENT);
+        menuBox.add(Box.createVerticalStrut(15));
+        menuBox.add(down);
+        down.setAlignmentX(Component.CENTER_ALIGNMENT);
+        menuBox.add(Box.createVerticalStrut(15));
+        menuBox.add(right);
+        right.setAlignmentX(Component.CENTER_ALIGNMENT);
+        menuBox.add(Box.createVerticalStrut(15));
+        menuBox.add(left);
+        left.setAlignmentX(Component.CENTER_ALIGNMENT);
+        add(menuBox);
+
+        rozpocznij.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                game();
+            }
+        });
+
+    }
+
+    void game(){
+        remove(menuBox);
+        setSize(417, 840);
+        setLocation((screenWidth-this.getWidth())/2, (screenHeight-this.getHeight())/2);
+        board = new Board();
+        boardPanel = new BoardPanel();
+        boardPanel.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_UP){
+                    board.rotate();
+                }
+                else if(e.getKeyCode() == KeyEvent.VK_LEFT){
+                    board.moveLeft();
+                }
+                else if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    board.moveRight();
+                }
+                else if(e.getKeyCode() == KeyEvent.VK_DOWN){
+                    board.moveDown();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+        add(boardPanel);
+        boardPanel.setFocusable(true);
+        boardPanel.requestFocus();
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(!(board.isLost())) {
+                    boardPanel.repaint();
+                    try {
+                        sleep(100);
+                    } catch (InterruptedException exc) {
+                        System.out.println(exc.getMessage());
+                    }
+                }
+                score = board.getPoints();
+                saveScore();
+                remove(boardPanel);
+                menu();
+            }
+        });
+        thread.start();
+
+    }
+
+    void saveScore(){
+        if(score>highScore){
+            highScore=score;
+            try(FileOutputStream Fout = new FileOutputStream(new File("highestScore.txt"))){
+                Fout.write((byte)score);
+            }
+            catch(IOException exc){
+                System.out.println(exc.getMessage());
+            }
+        }
+
+    }
+
+    void loadScore(){
+        try(FileInputStream Fin = new FileInputStream(new File("highestScore.txt"))) {
+            highScore = (int)(Fin.read());
+        }
+        catch(IOException exc){
+            System.out.println(exc.getMessage());
+        }
+    }
+
+    public static void START(){
+        Tetris frame = new Tetris();
+        frame.screenHeight=Toolkit.getDefaultToolkit().getScreenSize().height;
+        frame.screenWidth=Toolkit.getDefaultToolkit().getScreenSize().width;
+        frame.loadScore();
+        frame.menu();
+    }
+
+
+
+    class BoardPanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            setBackground(new Color(60, 60, 60));
+            g.setColor(new Color(255, 0, 0));
+            g.drawLine(0, 100, 400,100);
+            g.setColor(new Color(0,255,0));
+            g.drawString("Wynik: "+ board.getPoints(), 345, 17);
+            boolean[][] currBoard = board.getBoard();
+            for(int i=0; i<16; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if(currBoard[i][j]) {
+                        g.setColor(new Color(170, 170, 0));
+                        g.fillRect(50*j, 50*i, 50, 50);
+                        g.setColor(new Color(255, 255, 255));
+                        g.drawRect(50*j, 50*i, 50, 50);
+                    }
+                }
+            }
+        }
+    }
+
+}
